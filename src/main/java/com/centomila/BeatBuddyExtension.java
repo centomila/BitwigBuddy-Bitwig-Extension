96d6d42d853c5f14895b9398e3770625b3f2abd2
centomila <26800547+centomila@users.noreply.github.com>
2025-01-31 05:16:01 +0100

    basic 4 on the 4 working!

--- src/main/java/com/centomila/BeatBuddyExtension.java
+++ src/main/java/com/centomila/BeatBuddyExtension.java
@@ -11,14 +11,15 @@ import com.bitwig.extension.controller.api.DocumentState;
 import com.bitwig.extension.controller.api.EnumValue;
 import com.bitwig.extension.controller.api.Setting;
 
-public class BeatBuddyExtension extends ControllerExtension {
+public class BeatBuddyExtension extends ControllerExtension implements GmDrums {
    private Application application;
    private Clip cursorClip;
+   private Clip clip;
    private CursorTrack cursorTrack;
    private DocumentState documentState;
    private Setting genreSelector;
    private Setting clipLength;
-   private Setting quantization;
+   private Setting noteDuration;
    private Setting velocityVariation;
    private Setting humanization;
 
@@ -35,21 +36,23 @@ public class BeatBuddyExtension extends ControllerExtension {
 
       // Initialize API objects
       application = host.createApplication();
-      cursorClip = host.createLauncherCursorClip(8, 128);
-      cursorTrack = host.createCursorTrack(0, 0);
+      cursorClip = host.createLauncherCursorClip((16 * 8), 128);
+      cursorTrack = host.createCursorTrack((16 * 8), 128);
       documentState = host.getDocumentState();
 
-      final String[] QUANTIZATION_OPTIONS = new String[] { "1/4", "1/8", "1/16", "1/32", "1/64", "1/128" };
+      final String[] NOTEDURATION_OPTIONS = new String[] { "1/4", "1/8", "1/16", "1/32", "1/64", "1/128" };
       // Define pattern settings
       String[] genres = new String[] { "Techno", "House", "Deep House", "Dubstep" };
       
       genreSelector = (Setting) documentState.getEnumSetting("Genre", "Genre", genres, "Techno");
       
       clipLength = (Setting) documentState.getNumberSetting("Clip Length (Bars)", "Clip", 1, 16, 4, "Bar(s)", 1);
-      quantization = (Setting) documentState.getEnumSetting("Quantization", "Clip", QUANTIZATION_OPTIONS, "1/16");
+      noteDuration = (Setting) documentState.getEnumSetting("noteDuration", "Clip", NOTEDURATION_OPTIONS, "1/16");
       
       velocityVariation = (Setting) documentState.getBooleanSetting("Velocity Variation", "Clip", true);
       humanization = (Setting) documentState.getBooleanSetting("Humanization", "Clip", true);
+
+
 
       documentState.getSignalSetting("Generate!", "Generate", "Generate!").addSignalObserver(() -> {
          generateDrumPattern();
@@ -59,15 +62,40 @@ public class BeatBuddyExtension extends ControllerExtension {
 
    private void generateDrumPattern() {
       String selectedGenre = ((EnumValue) genreSelector).get(); // Get the current selected value of genreSelector
-      String selectedQuantization = ((EnumValue) quantization).get(); // Get the current selected value of quantization
-
+      String selectedNoteDuration = ((EnumValue) noteDuration).get(); // Get the current selected value of noteDuration
 
       String settingsString = "Genre: " + selectedGenre;
-      settingsString += " - Quantization: " + selectedQuantization;
+      settingsString += " - noteDuration: " + selectedNoteDuration;
 
       getHost().showPopupNotification(settingsString);
 
+      // channel - the channel of the new note
+      // x - the x position within the note grid, defining the step/time of the new note
+      // y - the y position within the note grid, defining the key of the new note. Use GMDrums constants to define the note number
+      // insertVelocity - the velocity of the new note. Default is 100
+      // insertDuration - the duration of the new note. Default is 1.
+
+      int channel = 0;
+      int x = 0;
+      int y = GmDrums.BASS_DRUM;
+      int insertVelocity = 100;
+      double insertDuration = 0.25;
+
+
+      cursorClip.setStep(channel, 0, y, insertVelocity, insertDuration);
+      cursorClip.setStep(channel+1, 4, y, insertVelocity, insertDuration);
+      cursorClip.setStep(channel+1, 8, y, insertVelocity, insertDuration);
+      cursorClip.setStep(channel+1, 12, y, insertVelocity, insertDuration);
+      
+
+      
+
+      // // Clear the cursor track
+      // cursorClip.setStep(x, y,velocity, duration);
+
    }
+   
+
 
    @Override
    public void exit() {
---
+++ src/main/java/com/centomila/GmDrums.java
@@ -0,0 +1,104 @@
+package com.centomila;
+
+public interface GmDrums {
+
+    void init();
+
+    final int ACOUSTIC_BASS_DRUM = 35;
+    final int BASS_DRUM = 36;
+    final int SIDE_STICK = 37;
+    final int ACOUSTIC_SNARE = 38;
+    final int HAND_CLAP = 39;
+    final int ELECTRIC_SNARE = 40;
+    final int LOW_FLOOR_TOM = 41;
+    final int CLOSED_HI_HAT = 42;
+    final int HIGH_FLOOR_TOM = 43;
+    final int PEDAL_HI_HAT = 44;
+    final int LOW_TOM = 45;
+    final int OPEN_HI_HAT = 46;
+    final int LOW_MID_TOM = 47;
+    final int HI_MID_TOM = 48;
+    final int CRASH_CYMBAL_1 = 49;
+    final int HIGH_TOM = 50;
+    final int RIDE_CYMBAL_1 = 51;
+    final int CHINESE_CYMBAL = 52;
+    final int RIDE_BELL = 53;
+    final int TAMBOURINE = 54;
+    final int SPLASH_CYMBAL = 55;
+    final int COWBELL = 56;
+    final int CRASH_CYMBAL_2 = 57;
+    final int VIBRASLAP = 58;
+    final int RIDE_CYMBAL_2 = 59;
+    final int HI_BONGO = 60;
+    final int LO_BONGO = 61;
+    final int MUTE_HI_CONGA = 62;
+    final int OPEN_HI_CONGA = 63;
+    final int LOW_CONGA = 64;
+    final int HIGH_TIMBALE = 65;
+    final int LOW_TIMBALE = 66;
+    final int HIGH_AGOGO = 67;
+    final int LOW_AGOGO = 68;
+    final int CABASA = 69;
+    final int MARACAS = 70;
+    final int SHORT_WHISTLE = 71;
+    final int LONG_WHISTLE = 72;
+    final int SHORT_GUIRO = 73;
+    final int LONG_GUIRO = 74;
+    final int CLAVES = 75;
+    final int HIGH_WOOD_BLOCK = 76;
+    final int LOW_WOOD_BLOCK = 77;
+    final int MUTE_CUICA = 78;
+    final int OPEN_CUICA = 79;
+    final int MUTE_TRIANGLE = 80;
+    final int OPEN_TRIANGLE = 81;
+
+}
+
+// Note # 	Note 	Drum Sound
+// 35 	B0 	Acoustic Bass Drum
+// 36 	C1 	Bass Drum
+// 37 	C#1 	Side Stick
+// 38 	D1 	Acoustic Snare
+// 39 	D#1 	Hand Clap
+// 40 	E1 	Electric Snare
+// 41 	F1 	Low Floor Tom
+// 42 	F#1 	Closed Hi Hat
+// 43 	G1 	High Floor Tom
+// 44 	G#1 	Pedal Hi-Hat
+// 45 	A1 	Low Tom
+// 46 	A#1 	Open Hi-Hat
+// 47 	B1 	Low-Mid Tom
+// 48 	C2 	Hi-Mid Tom
+// 49 	C#2 	Crash Cymbal 1
+// 50 	D2 	High Tom
+// 51 	D#2 	Ride Cymbal 1
+// 52 	E2 	Chinese Cymbal
+// 53 	F2 	Ride Bell
+// 54 	F#2 	Tambourine
+// 55 	G2 	Splash Cymbal
+// 56 	G#2 	Cowbell
+// 57 	A2 	Crash Cymbal 2
+// 58 	A#2 	Vibraslap
+// 59 	B2 	Ride Cymbal 2
+// 60 	C3 	Hi Bongo
+// 61 	C#3 	Low Bongo
+// 62 	D3 	Mute Hi Conga
+// 63 	D#3 	Open Hi Conga
+// 64 	E3 	Low Conga
+// 65 	F3 	Hi Timbale
+// 66 	F#3 	Low Timbale
+// 67 	G3 	Hi Agogo
+// 68 	G#3 	Low Agogo
+// 69 	A3 	Cabasa
+// 70 	A#3 	Maracas
+// 71 	B3 	Short Whistle
+// 72 	C4 	Long Whistle
+// 73 	C#4 	Short Guiro
+// 74 	D4 	Long Guiro
+// 75 	D#4 	Claves
+// 76 	E4 	Hi Wood Block
+// 77 	F4 	Low Wood Block
+// 78 	F#4 	Mute Cuica
+// 79 	G4 	Open Cuica
+// 80 	G#4 	Mute Triangle
+// 81 	A4 	Open Triangle
