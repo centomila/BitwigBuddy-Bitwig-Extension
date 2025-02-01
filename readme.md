d94ca614970449f17b37d0de44d1ad1262921310
centomila <26800547+centomila@users.noreply.github.com>
2025-02-01 01:32:44 +0100

    compiled version 0.3

---
+++ readme.md
@@ -0,0 +1,9 @@
+Version 0.3
+
+Installation:
+[] Download BeatBuddy.bwextension
+[] Copy to /Bitwig Studio/Extensions folder
+[] Settings > Controller > Add Extension > Centomila > BeatBuddy
+
+TODO:
+[] Readme!
--- src/main/java/com/centomila/BeatBuddyExtensionDefinition.java
+++ src/main/java/com/centomila/BeatBuddyExtensionDefinition.java
@@ -29,7 +29,7 @@ public class BeatBuddyExtensionDefinition extends ControllerExtensionDefinition
    @Override
    public String getVersion()
    {
-      return "0.2";
+      return "0.3";
    }
 
    @Override
