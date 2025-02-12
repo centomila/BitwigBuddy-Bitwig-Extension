7c1d823edf5b663b43d96da08440c01977ff81c4
centomila <26800547+centomila@users.noreply.github.com>
2025-02-12 16:57:18 +0100

    note destination files merged into one

--- src/main/java/com/centomila/NoteDestinationSettings.java
+++ src/main/java/com/centomila/NoteDestinationSettings.java
@@ -3,6 +3,8 @@ package com.centomila;
 import com.bitwig.extension.controller.api.ControllerHost;
 import com.bitwig.extension.controller.api.SettableRangedValue;
 import com.bitwig.extension.controller.api.Setting;
+import com.bitwig.extension.controller.api.EnumValue;
+import java.util.Arrays;
 
 public class NoteDestinationSettings {
     private final ControllerHost host;
@@ -40,5 +42,45 @@ public class NoteDestinationSettings {
         this.currentOctaveAsInt = octave;
         getCurrentNoteDestinationAsInt();
         notifyNoteDestination();
+    }
+}
+
+class NoteDestinationInitializer {
+    public static void initNoteDestinationSetting(BeatBuddyExtension extension) {
+        // Retrieve document state from extension
+        var documentState = extension.getDocumentState();
+        var host = extension.getHost();
+
+        // Note destination dropdown
+        String[] NOTEDESTINATION_OPTIONS = Utils.NOTE_NAMES;
+        extension.setNoteDestinationSetting((Setting) documentState.getEnumSetting("Note Destination", "Note Destination",
+                NOTEDESTINATION_OPTIONS, NOTEDESTINATION_OPTIONS[0]));
+
+        // Note OCT destination dropdown
+        String[] OCTAVEDESTINATION_OPTIONS = Arrays.stream(Utils.NOTE_OCTAVES)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+        extension.setNoteOctaveSetting((Setting) documentState.getEnumSetting("Note Octave", "Note Destination",
+                OCTAVEDESTINATION_OPTIONS, OCTAVEDESTINATION_OPTIONS[3]));
+
+        extension.setNoteChannelSetting((Setting) documentState.getNumberSetting("Note Channel", "Note Destination",
+                1, 16, 1, "Channel MIDI", 1));
+
+        // Initialize NoteDestinationSettings and set in extension
+        extension.setNoteDestSettings(new NoteDestinationSettings(host, extension.getNoteChannelSetting(),
+                NOTEDESTINATION_OPTIONS[0], 3));
+
+        ((EnumValue) extension.getNoteDestinationSetting()).addValueObserver(newValue -> {
+            extension.getNoteDestSettings().setCurrentNote(newValue);
+        });
+
+        ((EnumValue) extension.getNoteOctaveSetting()).addValueObserver(newValue -> {
+            extension.getNoteDestSettings().setCurrentOctave(Integer.parseInt(newValue));
+        });
+
+        // Empty string for spacing
+        extension.setSpacer2((Setting) documentState.getStringSetting("----", "Clip", 0,
+                "---------------------------------------------------"));
+        extension.getSpacer2().disable();
     }
 }
