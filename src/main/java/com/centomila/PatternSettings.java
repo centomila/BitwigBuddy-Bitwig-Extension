28817eae05e6dd093246e09e199d2e1f83a1967a
centomila <26800547+centomila@users.noreply.github.com>
2025-02-13 03:21:19 +0100

    pattern settings more readable

--- src/main/java/com/centomila/PatternSettings.java
+++ src/main/java/com/centomila/PatternSettings.java
@@ -4,10 +4,7 @@ import com.bitwig.extension.controller.api.DocumentState;
 import com.bitwig.extension.controller.api.EnumValue;
 import com.bitwig.extension.controller.api.Setting;
 import com.bitwig.extension.controller.api.Signal;
-import com.bitwig.extension.controller.api.StringValue;
-import com.centomila.BeatBuddyPreferences;
 import com.centomila.BeatBuddyPreferences.CustomPreset;
-
 import java.util.Arrays;
 
 public class PatternSettings {
@@ -24,80 +21,111 @@ public class PatternSettings {
 
     private void initPatternSetting() {
         DocumentState documentState = extension.getDocumentState();
+        initGenerateButton(documentState);
+        initPatternTypeSetting(documentState);
+        initPatternSelectorSetting(documentState);
+        initCustomPresetSetting(documentState);
+        initReversePatternSetting(documentState);
+        initSpacer(documentState);
+    }
 
-        // Generate button
+    private void initGenerateButton(DocumentState documentState) {
         Signal generateButton = documentState.getSignalSetting("Generate!", "Generate", "Generate!");
+        generateButton.addSignalObserver(extension::generateDrumPattern);
+    }
 
-        generateButton.addSignalObserver(() -> {
-            extension.generateDrumPattern();
-        });
-
-        // Pattern type selector
-        extension.setPatternTypeSetting((Setting) documentState.getEnumSetting("Pattern Type", "Generate",
-                new String[] { "Presets", "Random", "Custom" }, "Presets"));
+    private void initPatternTypeSetting(DocumentState documentState) {
+        String[] options = { "Presets", "Random", "Custom" };
+        extension.setPatternTypeSetting((Setting) documentState.getEnumSetting("Pattern Type", "Generate", options, "Presets"));
 
         ((EnumValue) extension.getPatternTypeSetting()).addValueObserver(newValue -> {
             switch (newValue) {
                 case "Presets":
-                    extension.getPatternSelectorSetting().enable();
-                    extension.getPatternSelectorSetting().show();
-                    extension.getCustomPresetSetting().disable();
-                    extension.getCustomPresetSetting().hide();
-                    extension.getReversePatternSetting().enable();
-                    extension.getReversePatternSetting().show();
+                    showPatternSetting();
+                    hideCustomPresetSetting();
+                    showReversePatternSetting();
                     break;
                 case "Custom":
-                    extension.getCustomPresetSetting().enable();
-                    extension.getCustomPresetSetting().show();
-                    extension.getPatternSelectorSetting().disable();
-                    extension.getPatternSelectorSetting().hide();
-                    extension.getReversePatternSetting().enable();
-                    extension.getReversePatternSetting().show();
+                    hidePatternSetting();
+                    showCustomPresetSetting();
+                    showReversePatternSetting();
                     break;
                 case "Random":
-                    extension.getPatternSelectorSetting().disable();
-                    extension.getPatternSelectorSetting().hide();
-                    extension.getCustomPresetSetting().disable();
-                    extension.getCustomPresetSetting().hide();
-                    extension.getReversePatternSetting().disable();
-                    extension.getReversePatternSetting().hide();
+                    hidePatternSetting();
+                    hideCustomPresetSetting();
+                    hideReversePatternSetting();
                     break;
             }
         });
+    }
 
-        // Define pattern settings
+    private void initPatternSelectorSetting(DocumentState documentState) {
         final String[] PATTERN_OPTIONS = Arrays.stream(DrumPatterns.patterns)
                 .map(pattern -> pattern[0].toString())
                 .toArray(String[]::new);
+
         extension.setPatternSelectorSetting(
-                (Setting) documentState.getEnumSetting("Pattern", "Generate", PATTERN_OPTIONS,
-                        "Kick: Four on the Floor"));
+                (Setting) documentState.getEnumSetting("Pattern", "Generate", PATTERN_OPTIONS, "Kick: Four on the Floor"));
         ((EnumValue) extension.getPatternSelectorSetting()).addValueObserver(newValue -> {
             PopupUtils.showPopup(newValue.toString());
         });
+    }
 
-        // New Custom Presets dropdown (for Custom)
-
-        extension.setCustomPresetSetting((Setting) documentState.getEnumSetting("Custom Presets", "Generate",
-                getCustomPresetsContentNameStrings(), getCustomPresetsContentNameStrings()[0]));
+    private void initCustomPresetSetting(DocumentState documentState) {
+        String[] presets = getCustomPresetsContentNameStrings();
+        extension.setCustomPresetSetting((Setting) documentState.getEnumSetting("Custom Presets", "Generate", presets, presets[0]));
         extension.getCustomPresetSetting().disable();
         ((EnumValue) extension.getCustomPresetSetting()).addValueObserver(newValue -> {
             PopupUtils.showPopup("Custom Preset selected: " + newValue.toString());
         });
-
-        extension.setReversePatternSetting((Setting) documentState.getEnumSetting("Reverse Pattern", "Generate",
-                new String[] { "Normal", "Reverse" }, "Normal"));
-
-        // Empty string for spacing
-        extension.setSpacer1((Setting) documentState.getStringSetting("----", "Generate", 0,
-                "---------------------------------------------------"));
-        extension.getSpacer1().disable();
     }
-    
+
+    private void initReversePatternSetting(DocumentState documentState) {
+        extension.setReversePatternSetting((Setting) documentState.getEnumSetting("Reverse Pattern", "Generate", 
+                new String[] { "Normal", "Reverse" }, "Normal"));
+    }
+
+    private void initSpacer(DocumentState documentState) {
+        Setting spacer = (Setting) documentState.getStringSetting("----", "Generate", 0,
+                "---------------------------------------------------");
+        spacer.disable();
+        extension.setSpacer1(spacer);
+    }
+
+    // Helper methods for showing/hiding settings
+    private void showPatternSetting() {
+        extension.getPatternSelectorSetting().enable();
+        extension.getPatternSelectorSetting().show();
+    }
+
+    private void hidePatternSetting() {
+        extension.getPatternSelectorSetting().disable();
+        extension.getPatternSelectorSetting().hide();
+    }
+
+    private void showCustomPresetSetting() {
+        extension.getCustomPresetSetting().enable();
+        extension.getCustomPresetSetting().show();
+    }
+
+    private void hideCustomPresetSetting() {
+        extension.getCustomPresetSetting().disable();
+        extension.getCustomPresetSetting().hide();
+    }
+
+    private void showReversePatternSetting() {
+        extension.getReversePatternSetting().enable();
+        extension.getReversePatternSetting().show();
+    }
+
+    private void hideReversePatternSetting() {
+        extension.getReversePatternSetting().disable();
+        extension.getReversePatternSetting().hide();
+    }
+
     private String[] getCustomPresetsContentNameStrings() {
         return Arrays.stream(extension.getPreferences().getCustomPresets())
                      .map(CustomPreset::getName)
                      .toArray(String[]::new);
     }
-
 }
