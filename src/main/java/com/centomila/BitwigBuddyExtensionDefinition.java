8d578276ef0eccd4ac77f3d7d702546fee98b91a
centomila <26800547+centomila@users.noreply.github.com>
2025-04-07 19:18:36 +0200

    chore: update version to 1.0.0 alpha 2 in POM files and extension definition

--- src/main/java/com/centomila/BitwigBuddyExtensionDefinition.java
+++ src/main/java/com/centomila/BitwigBuddyExtensionDefinition.java
@@ -29,7 +29,7 @@ public class BitwigBuddyExtensionDefinition extends ControllerExtensionDefinitio
    @Override
    public String getVersion()
    {
-      return "1.0.0 alpha 1";
+      return "1.0.0 alpha 2";
    }
 
    @Override
